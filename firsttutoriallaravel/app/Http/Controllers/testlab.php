e5053bb7e0791eb7c03b9a3708ef7f3bffc50264
codefreezing <codefreezing@gmail.com>
2022-06-24 16:24:16 +0200

    merge with h

--- firsttutoriallaravel/app/Http/Controllers/testlab.php
+++ firsttutoriallaravel/app/Http/Controllers/testlab.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class testlab extends Controller
 {
-    public function index($id)
+    public function index($id)tygcfhhg
     {
         return "this is m lab";tui la tim
     }
